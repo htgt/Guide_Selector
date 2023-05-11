0e7792a71ed95875efc8396a289344471e8b29a6
Anna Shtanova <as63@sanger.ac.uk>
2023-05-11 13:05:44 +0000

    TD-396 Set up cli and argument parser

---
+++ src/cli.py
@@ -0,0 +1,29 @@
+import sys
+
+from utils.arguments_parser import ParsedInputArguments
+from mutator.mutator import Mutator
+
+def version_command():
+    python_version = sys.version
+    version = '0.0.1'
+
+    print('Guide Selection version: ', version)
+    print('Python version: ', python_version)
+
+
+def resolve_command(args):
+    command = args['command']
+
+    if command == 'version':
+        version_command()
+
+
+def main():
+    parsed_input = ParsedInputArguments()
+    args = parsed_input.get_args()
+
+    resolve_command(args)
+
+
+if __name__ == '__main__':
+    main()
