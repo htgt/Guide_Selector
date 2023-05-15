fe5c40815a054e74f030270038870cf0447d8d35
Anna Shtanova <as63@sanger.ac.uk>
2023-05-15 13:21:23 +0000

    TD-396 Change InputArguments arguments to dict

--- src/utils/arguments_parser.py
+++ src/utils/arguments_parser.py
@@ -1,9 +1,9 @@
-import argparse
+from argparse import ArgumentParser
 
 
 class InputArguments:
     def __init__(self) -> None:
-        self.arguments = []
+        self.arguments = {}
         self.command = ''
 
         self.parse_arguments()
@@ -13,7 +13,7 @@ class InputArguments:
         self.command = self.arguments['command']
 
     def parse_arguments(self) -> dict:
-        parser = argparse.ArgumentParser(
+        parser = ArgumentParser(
             description='Guide Selection CLI',
             prog='Guide Selection'
         )
@@ -23,11 +23,10 @@ class InputArguments:
             action='version',
             version='%(prog)s 0.0.1'
         )
-
         parser = add_input_args(parser)
 
         self.set_args(vars(parser.parse_args()))
 
 
-def add_input_args(parser):
+def add_input_args(parser) -> ArgumentParser:
     return parser
