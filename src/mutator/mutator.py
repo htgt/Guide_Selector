c8156532415b221f155b50c122111481e8b63c19
Beth Sampher <bs15@sanger.ac.uk>
2023-06-09 15:32:48 +0100

    TD-391 Determine guide frame relative to CDS

--- src/mutator/mutator.py
+++ src/mutator/mutator.py
@@ -10,7 +10,11 @@ from utils.exceptions import MutatorError
 class Mutator:
     def mutate(gtf: str, guide_tsv: str) -> None:
         gtf_data, guide_data = Mutator.read_input_files(gtf, guide_tsv)
-        print(Mutator.get_coding_regions_for_all_guides(gtf_data, guide_data))
+        coding_regions = Mutator.get_coding_regions_for_all_guides(gtf_data, guide_data)
+        coding_regions['guide_frame'] = coding_regions.apply(
+            Mutator.determine_frame_for_guide, axis=1
+        )
+        print(Mutator.adjust_columns_for_output(coding_regions))
 
     def read_input_files(gtf: str, guide_tsv: str) -> Tuple[List[dict], pd.DataFrame]:
         gtf_data = pr.read_gtf(gtf, as_df=True)
@@ -19,22 +23,14 @@ class Mutator:
         return (gtf_data, guide_data)
 
     def get_coding_regions_for_all_guides(
-        gtf_data: pd.DataFrame, guide_data: Tuple[List[dict]]
+        gtf_data: pd.DataFrame, guide_data: List[dict]
     ) -> pd.DataFrame:
         coding_regions = pd.DataFrame()
         for guide in guide_data:
             coding_region = Mutator.get_coding_region_for_guide(gtf_data, guide)
+            coding_region = Mutator.add_guide_data_to_dataframe(coding_region, guide)
             coding_regions = pd.concat([coding_regions, coding_region])
-        required_cols = [
-            'Chromosome',
-            'Start',
-            'End',
-            'gene_name',
-            'exon_number',
-            'Frame',
-            'Strand'
-        ]
-        return coding_regions[required_cols].copy()
+        return coding_regions
 
     def get_coding_region_for_guide(gtf_data: pd.DataFrame, guide: dict) -> pd.DataFrame:
         feature_cond = gtf_data['Feature'] == 'CDS'
@@ -50,6 +46,49 @@ class Mutator:
             raise MutatorError(
                 f'Guide {guide["guide_id"]} overlaps with multiple coding regions'
             )
-        coding_region['guide_id'] = int(guide['guide_id'])
-        coding_region.set_index('guide_id', inplace=True)
         return coding_region
+
+    def add_guide_data_to_dataframe(dataframe: pd.DataFrame, guide: dict) -> pd.DataFrame:
+        dataframe = dataframe.copy()
+        dataframe['guide_id'] = int(guide['guide_id'])
+        dataframe.set_index('guide_id', inplace=True)
+        dataframe['guide_start'] = int(guide['start'])
+        dataframe['guide_end'] = int(guide['end'])
+        return dataframe
+
+    def determine_frame_for_guide(row: pd.Series) -> str:
+        if row['Strand'] == '+':
+            if row['guide_start'] < row['Start']:
+                return row['Frame']
+            difference = row['guide_start'] - row['Start']
+        else:
+            if row['guide_end'] > row['End']:
+                return row['Frame']
+            difference = row['End'] - row['guide_end']
+        frames = ('0', '2', '1')
+        return frames[(difference + int(frames.index(row['Frame']))) % 3]
+
+    def adjust_columns_for_output(coding_regions: pd.DataFrame) -> pd.DataFrame:
+        coding_regions.rename(
+            columns={
+                'Chromosome': 'chromosome',
+                'Start': 'cds_start',
+                'End': 'cds_end',
+                'Strand': 'cds_strand',
+                'Frame': 'cds_frame',
+            },
+            inplace=True
+        )
+        required_cols = [
+            'chromosome',
+            'cds_start',
+            'cds_end',
+            'cds_strand',
+            'cds_frame',
+            'gene_name',
+            'exon_number',
+            'guide_start',
+            'guide_end',
+            'guide_frame',
+        ]
+        return coding_regions[required_cols].copy()
