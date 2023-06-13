e0c2fa5867e77032676e51df94e4a71452713846
Anna Shtanova <as63@sanger.ac.uk>
2023-06-13 13:09:33 +0000

    TD-392 Add tests for find_pam

---
+++ tests/mutator/test_guide.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+
+from mutator.guide import GuideSequence
+
+class TestGuideSequence(TestCase):
+    def setUp(self):
+        positive_bases = "AGTTTCGGACTCCTCCACAATGGCT"
+        self.test_positive_guide = GuideSequence(positive_bases, "+")
+
+        pam = "TGG"
+        pam_start = 20
+        pam_end = 23
+        self.pam_position = {
+            "start": pam_start,
+            "end": pam_end
+        }
+
+        negative_bases = "GCCATTGTGGAGGAGTCCGAAACT"
+        self.test_negative_guide = GuideSequence(negative_bases, "-")
+
+
+    def test_guide_find_pam_positive_strand(self):
+        guide = self.test_positive_guide
+
+        test_pam = guide.find_pam()
+
+        self.assertEqual(test_pam, self.pam_position)
+
+    def test_guide_find_pam_negative_strand(self):
+        pam_start =1
+        pam_end = 4
+        pam_position = {
+            "start": pam_start,
+            "end": pam_end
+        }
+
+        guide = self.test_negative_guide
+
+        test_pam = guide.find_pam()
+
+        self.assertEqual(test_pam, pam_position)
+
+
+    def test_define_window_positive_strand(self):
+        window_start = 11
+        window_end = 23
+        window = {
+            "start": window_start,
+            "end": window_end
+        }
+
+        guide = self.test_positive_guide
+
+        test_window = guide.define_window_positive_strand(self.pam_position)
+
+        self.assertEqual(test_window, window)
+
+
+
