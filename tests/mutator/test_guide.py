0fa02b2f23a6782be6b9dfc34f757fd02b7e79c8
Anna Shtanova <as63@sanger.ac.uk>
2023-06-13 13:57:03 +0000

    TD-392 Use SequenceFragment class for pam parameter of GuideSequence class

--- tests/mutator/test_guide.py
+++ tests/mutator/test_guide.py
@@ -6,19 +6,11 @@ class TestGuideSequence(TestCase):
     def setUp(self):
         positive_bases = "AGTTTCGGACTCCTCCACAATGGCT"
         self.test_positive_guide = GuideSequence(positive_bases, "+")
-
-        pam = "TGG"
-        pam_start = 20
-        pam_end = 23
-        self.pam_fragment_positive = SequenceFragment(pam, pam_start, pam_end)
-
-        self.pam_position = {
-            "start": pam_start,
-            "end": pam_end
-        }
+        self.pam_fragment_positive = SequenceFragment("TGG", 20, 23)
 
         negative_bases = "GCCATTGTGGAGGAGTCCGAAACT"
         self.test_negative_guide = GuideSequence(negative_bases, "-")
+        self.pam_fragment_negative = SequenceFragment("CCA", 1, 4)
 
 
     def test_guide_find_pam_positive_strand(self):
@@ -29,29 +21,19 @@ class TestGuideSequence(TestCase):
         self.assertEqual(test_pam, self.pam_fragment_positive)
 
     def test_guide_find_pam_negative_strand(self):
-        pam = "CCA"
-        pam_start =1
-        pam_end = 4
-        pam_fragment = SequenceFragment(pam, pam_start, pam_end)
-
         guide = self.test_negative_guide
 
         test_pam = guide.find_pam()
 
-        self.assertEqual(test_pam, pam_fragment)
+        self.assertEqual(test_pam, self.pam_fragment_negative)
 
 
     def test_define_window_positive_strand(self):
-        window_start = 11
-        window_end = 23
-        window = {
-            "start": window_start,
-            "end": window_end
-        }
+        window = SequenceFragment("CCTCCACAATGG", 11, 23)
 
         guide = self.test_positive_guide
 
-        test_window = guide.define_window_positive_strand(self.pam_position)
+        test_window = guide.define_window_positive_strand()
 
         self.assertEqual(test_window, window)
 
