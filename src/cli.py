6ae96ac14d38223666c279eeb2aebdc1bb597d0c
Anna Shtanova <as63@sanger.ac.uk>
2023-06-22 13:46:44 +0000

    TD-393 Get window frame and extended window coordinates

--- src/cli.py
+++ src/cli.py
@@ -3,21 +3,23 @@ import sys
 from utils.arguments_parser import InputArguments
 from mutator.mutator import Mutator
 from mutator.guide import GuideSequence
-
+from mutator.window import get_window_frame
 
 def resolve_command(command: str, args: dict) -> None:
     if command == "window":
         guide = GuideSequence(args["seq"], args["strand"], args["window_length"])
 
         print(guide.window)
-    
+
 
 def main() -> None:
     parsed_input = InputArguments()
     args = parsed_input.arguments
     command = parsed_input.command
 
-    resolve_command(command, args)
+    get_window_frame()
+
+   # resolve_command(command, args)
 
 
 if __name__ == '__main__':
