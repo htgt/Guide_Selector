2b3098b61116946b31930e66ce99dc0b04e8469d
pk8 <pk8@sanger.ac.uk>
2023-07-03 15:51:04 +0000

    Moved file handling to cli

--- src/cli (3 Jul 2023 at 16-50-40).py
+++ src/cli (3 Jul 2023 at 16-50-40).py
@@ -4,8 +4,6 @@ from utils.arguments_parser import InputArguments
 from mutator.mutator import Mutator
 from mutator.guide import GuideSequence
 from mutator.mutation_builder import get_window_frame
-from mutator.data_adapter import build_coding_region_objects
-from utils.file_system import read_csv_to_list_dict
 
 def resolve_command(command: str, args: dict) -> None:
     if command == "window":
@@ -19,10 +17,7 @@ def main() -> None:
     args = parsed_input.arguments
     command = parsed_input.command
 
-    file_data = read_csv_to_list_dict(file, "\t")
-    for row in (file_data):
-        cds, window = build_coding_region_objects(row)
-        get_window_frame(cds, window)
+    get_window_frame(args["file"])
 
    # resolve_command(command, args)
 
