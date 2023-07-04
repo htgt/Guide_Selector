7b0cac8156168263a705c04674767b7adf6cf54e
Anna Shtanova <as63@sanger.ac.uk>
2023-07-04 13:29:41 +0000

    TD-393 Fix extended window coordinates

--- tests/mutator/test_edit_window.py
+++ tests/mutator/test_edit_window.py
@@ -4,18 +4,16 @@ import unittest
 from parameterized import parameterized
 
 
-
-
-class TestParametrizedEditWindow(unittest.TestCase):
+class TestEditWindowExtendedCoordinates(unittest.TestCase):
     @parameterized.expand([
         # Positive strand test cases
         (10, 21, True, '16', 0, (10, 21)),
-        (10, 21, True, '16', 1, (9, 21)),
-        (10, 21, True, '16', 2, (8, 21)),
+        (10, 21, True, '16', 1, (8, 21)),
+        (10, 21, True, '16', 2, (9, 21)),
         # Negative strand test cases
         (10, 21, False, '16', 0, (10, 21)),
-        (10, 21, False, '16', 1, (10, 22)),
-        (10, 21, False, '16', 2, (10, 23)),
+        (10, 21, False, '16', 1, (10, 23)),
+        (10, 21, False, '16', 2, (10, 22)),
     ])
     def test_get_extended_window_coordinates(self, start, end, isPositiveStrand, chromosome, frame, expected_coordinates):
         window = EditWindow(start, end, isPositiveStrand, chromosome, frame)
@@ -23,6 +21,7 @@ class TestParametrizedEditWindow(unittest.TestCase):
         result_coordinates = window._get_extended_window_coordinates()
 
         self.assertEqual(result_coordinates, expected_coordinates, "Incorrect extended window coordinates")
+
 
 class TestEditWindowCodons(unittest.TestCase):
         def test_split_window_into_codons(self):
