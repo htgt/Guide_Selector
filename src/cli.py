c16c56160a881281448a4b00daef4fb5d48876d5
Anna Shtanova <as63@sanger.ac.uk>
2023-07-05 14:33:05 +0000

    TD-393 Fix EditWindow to use extended coordinates

--- src/cli.py
+++ src/cli.py
@@ -1,42 +1,17 @@
 import sys
 
-from dataclasses import dataclass
-
 from utils.arguments_parser import InputArguments
 from mutator.mutator import Mutator
 from mutator.guide import GuideSequence
 from mutator.mutation_builder import get_window_frame
 from mutator.data_adapter import build_coding_region_objects
-from mutator.base_sequence import BaseSequence
-from mutator.edit_window import EditWindow, WindowCodon
 from utils.file_system import read_csv_to_list_dict
 
-
-@dataclass
-class Runner:
-    cds: BaseSequence
-    window: EditWindow
-    codon: WindowCodon
-    guide: GuideSequence
-
-    def __init__(self):
-        self.cds = None
-        self.window = None
-        self.codon = None
-        self.guide = None
-
-
-    def window_frame(self, input_file_path):
-        file_data = read_csv_to_list_dict(input_file_path, "\t")
-        for row in (file_data):
-            self.cds, self.window = build_coding_region_objects(row)
-            get_window_frame(self.cds, self.window)
-
-
 def resolve_command(command: str, args: dict) -> None:
-    runner = Runner()
     if command == "window":
-        runner.window_frame(args['file'])
+        guide = GuideSequence(args["seq"], args["strand"], args["window_length"])
+
+        print(guide.window)
 
 
 def main() -> None:
@@ -44,7 +19,12 @@ def main() -> None:
     args = parsed_input.arguments
     command = parsed_input.command
 
-    resolve_command(command, args)
+    file_data = read_csv_to_list_dict(file, "\t")
+    for row in (file_data):
+        cds, window = build_coding_region_objects(row)
+        get_window_frame(cds, window)
+
+   # resolve_command(command, args)
 
 
 if __name__ == '__main__':
