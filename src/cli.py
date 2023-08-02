6d5c171af1dc6ff79344142a2391c0ee91f4181b
Peter Inzani <pi2@sanger.ac.uk>
2023-08-02 16:38:02 +0100

    changed out to out_dir to match updates

--- src/cli.py
+++ src/cli.py
@@ -38,11 +38,11 @@ def run_mutator_cmd(args: dict, config: dict) -> None:
 
     # Write to VCF
     tsv_rows = runner.as_rows(config)
-    tsv_path = args['out'] + '/' + OUTPUT_TSV_FILE
+    tsv_path = args['out_dir'] + '/' + OUTPUT_TSV_FILE
     write_dict_list_to_csv(tsv_path, tsv_rows, tsv_rows[0].keys(), "\t")
     print('Output saved to', tsv_path)
 
-    vcf_path = args['out'] + '/' + OUTPUT_VCF_FILE
+    vcf_path = args['out_dir'] + '/' + OUTPUT_VCF_FILE
     runner.write_output_to_vcf(vcf_path)
     print('Output saved to', vcf_path)
 
