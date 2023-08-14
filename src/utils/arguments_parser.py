cab3b2ce12d71a7fc60b1f6bc6df0dd26a7cf285
Anna Shtanova <as63@sanger.ac.uk>
2023-08-14 12:58:13 +0000

    TD-453 Move common --config and --out-dir arguments from subparsers

--- src/utils/arguments_parser.py
+++ src/utils/arguments_parser.py
@@ -19,17 +19,31 @@ class InputArguments:
         )
 
         self._add_subparsers(parser)
+        self._add_input_args(parser)
 
+        self.set_args(vars(parser.parse_args()))
+
+    @staticmethod
+    def _add_input_args(parser) -> None:
         parser.add_argument(
             '--version',
             action='version',
-            version='%(prog)s 0.0.1'
+            version='%(prog)s 0.0.1',
         )
-
-
-        parser = add_input_args(parser)
-
-        self.set_args(vars(parser.parse_args()))
+        parser.add_argument(
+            '--conf',
+            type=str,
+            help='Path to custom configuration file',
+            default='',
+        )
+        parser.add_argument(
+            '--out_dir',
+            type=str,
+            nargs='?',
+            const='.',
+            default='./',
+            help='Desired output path (Default: ./)',
+        )
 
     def _add_subparsers(self, parser) -> None:
         subparsers = parser.add_subparsers(dest='command')
@@ -37,29 +51,16 @@ class InputArguments:
         self._add_mutator_command_parser(subparsers)
         self._add_retrieve_command_parser(subparsers)
 
-    def _add_mutator_command_parser(self, subparsers: _SubParsersAction) -> None:
+    @staticmethod
+    def _add_mutator_command_parser(subparsers: _SubParsersAction) -> None:
         parser_mutator = subparsers.add_parser('mutator', help='Mutator command help')
         parser_mutator.add_argument('--tsv', type=str,
             help='Path to Guide Locus as TSV file. Required columns: guide start, end, strand and id')
         parser_mutator.add_argument('--gtf', type=str, help='Path to reference GTF file')
-        parser_mutator.add_argument(
-            '--conf',
-            type=str,
-            help='Path to custom configuration file',
-            default='',
-        )
-        parser_mutator.add_argument(
-            '--out_dir',
-            type=str,
-            nargs='?',
-            const='.',
-            default='./',
-            help='Desired output path (Default: ./)'
-        )
 
-    def _add_retrieve_command_parser(self, subparsers: _SubParsersAction) -> None:
+    @staticmethod
+    def _add_retrieve_command_parser(subparsers: _SubParsersAction) -> None:
         parser_retrieve = subparsers.add_parser('retrieve', help='Retrieve command help')
 
 
-def add_input_args(parser) -> ArgumentParser:
-    return parser
+
