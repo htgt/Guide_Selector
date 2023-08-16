3ee997f4c51750ba507dcc67c465936182e5d3ff
Anna Shtanova <as63@sanger.ac.uk>
2023-08-16 15:44:51 +0000

    TD-453 Add test for get_guides_data

--- tests/mutator/test_retrieve.py
+++ tests/mutator/test_retrieve.py
@@ -1,7 +1,36 @@
 from unittest import TestCase
-from mutator.retrieve import parse_gff
+from unittest.mock import Mock, patch, call
+from mutator.retrieve import get_guides_data, retrieve_data_for_region, parse_gff
 
 class RetrieveModule(TestCase):
+
+    @patch('mutator.retrieve.retrieve_data_for_region')
+    def test_get_guides_data(self, mock_retrieve_data_for_region):
+        region1 = "chr19:111-222"
+        region2 = "chr2:123-155"
+        regions = [ {"id": "AAA", "region": region1}, {"id": "BBB", "region": region2} ]
+        config = {}
+
+        def return_function(region, config):
+            if region == region1:
+                return {'guide_id': '263969621', 'chr': 'chr19', 'start': 50398851, 'end': 50398873, 'grna_strand': '+', 'ot_summary': '263969621', 'seq': 'TGGGATGAAAAACGTGGGACAGG'},
+            if region == region2:
+                return {'guide_id': '22222', 'chr': 'chr2', 'start': 133, 'end': 150, 'grna_strand': '-', 'ot_summary': '1111', 'seq': 'TGGGAT'},
+
+        mock_retrieve_data_for_region.side_effect = return_function
+
+        result = get_guides_data(regions, config)
+
+        expected_result = [
+            {'guide_id': '263969621', 'chr': 'chr19', 'start': 50398851, 'end': 50398873, 'grna_strand': '+', 'ot_summary': '263969621', 'seq': 'TGGGATGAAAAACGTGGGACAGG'},
+            {'guide_id': '22222', 'chr': 'chr2', 'start': 133, 'end' : 150, 'grna_strand': '-', 'ot_summary': '1111', 'seq': 'TGGGAT'},
+        ]
+
+        self.assertEqual(result, expected_result)
+
+        calls = [call(region1, config), call(region2, config)]
+        mock_retrieve_data_for_region.assert_has_calls(calls)
+
     def test_parse_gff(self):
         gff_data = """##gff-version 3
     ##sequence-region lims2-region 48898521 48902973
