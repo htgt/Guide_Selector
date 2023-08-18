522324d581fa91fc7a7a4007dec48304cdd474fc
Peter Keen <pk8@sanger.ac.uk>
2023-08-18 14:27:01 +0100

    WGE species to grch38

--- src/utils/get_data/wge.py
+++ src/utils/get_data/wge.py
@@ -4,11 +4,11 @@ def get_data_from_wge_by_coords(
         chromosome: str,
         start: int,
         end: int,
-        species_id: str='Human',
+        species_id: str='GRCh38',
         assembly: str='GRCh38'
 ):
     base_url = "https://wge.stemcell.sanger.ac.uk/api/crisprs_in_region"
-    url = f"{base_url}?chr={chromosome}&start={start}&end={end}&species_id={species_id}&assembly={assembly}"
+    url = f"{base_url}?chr={chromosome}&start={start}&end={end}&species_id={assembly}&assembly={assembly}"
 
     headers = {'Content-type': 'text/plain'}
     response = requests.get(url, headers=headers)
