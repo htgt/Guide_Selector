6eccb42f749a07c0d3457fb0f3279bfd3487c0da
Miguel Rada <mr33@sanger.ac.uk>
2023-09-29 16:08:59 +0100

    TD-459test filter validator added

--- tests/filter/test_filter_manager.py
+++ tests/filter/test_filter_manager.py
@@ -10,7 +10,7 @@ from mutation_builder import MutationBuilder
 class TestFilterManager(unittest.TestCase):
     def setUp(self) -> None:
         self.filter_dict = {"filters": {"min_edits_allowed": 3}}
-        self.filter_class_to_load = MinimumEditsFilter
+        self.filter_to_load = MinimumEditsFilter
         mb_1codons = Mock()
         mb_1codons.codons = ["codon1"]
         mb_2codons = Mock()
@@ -21,43 +21,39 @@ class TestFilterManager(unittest.TestCase):
         mb_4codons.codons = ["codon1", "codon2", "codon3", "codon4"]
         self.mutations_builder_to_filter = [mb_1codons, mb_2codons, mb_3codons, mb_4codons]
 
+        self.test_instance = FilterManager(self.filter_dict)
+
     def test_load_filter(self):
-        test_instance = FilterManager(self.filter_dict)
+        self.assertDictEqual(self.test_instance._active_filters, {}, 'There is not active filters')
 
-        self.assertDictEqual(test_instance._active_filters, {}, 'There is not active filters')
+        self.test_instance.load_filter(self.filter_to_load)
 
-        test_instance.load_filter(self.filter_class_to_load)
-
-        # assert len(test_instance._active_filters) == 1
-        self.assertEqual(len(test_instance._active_filters), 1)
-        self.assertIsInstance(test_instance._active_filters["MinimumEditsFilter"], self.filter_class_to_load)
+        self.assertEqual(len(self.test_instance._active_filters), 1)
+        self.assertIsInstance(self.test_instance._active_filters["MinimumEditsFilter"], self.filter_to_load)
 
     def test_unload_filter_when_exists(self):
-        test_instance = FilterManager(self.filter_dict)
-        test_instance.load_filter(self.filter_class_to_load)
+        self.test_instance.load_filter(self.filter_to_load)
 
-        self.assertEqual(len(test_instance._active_filters), 1)
+        self.assertEqual(len(self.test_instance._active_filters), 1)
 
-        test_instance.unload_filter("MinimumEditsFilter")
+        self.test_instance.unload_filter("MinimumEditsFilter")
 
-        self.assertEqual(len(test_instance._active_filters), 0)
+        self.assertEqual(len(self.test_instance._active_filters), 0)
 
     def test_unload_filter_when_do_not_exists(self):
-        test_instance = FilterManager(self.filter_dict)
-        test_instance.load_filter(self.filter_class_to_load)
+        self.test_instance.load_filter(self.filter_to_load)
 
-        self.assertEqual(len(test_instance._active_filters), 1)
+        self.assertEqual(len(self.test_instance._active_filters), 1)
 
-        test_instance.unload_filter("Not Existing Filter Name")
+        self.test_instance.unload_filter("Not Existing Filter Name")
 
-        self.assertEqual(len(test_instance._active_filters), 1)
+        self.assertEqual(len(self.test_instance._active_filters), 1)
 
     def test_filter_data(self):
-        test_instance = FilterManager(self.filter_dict)
-        test_instance.load_filter(self.filter_class_to_load)
+        self.test_instance.load_filter(self.filter_to_load)
 
         self.assertEqual(len(self.mutations_builder_to_filter), 4)
 
-        result = test_instance.apply_filters(self.mutations_builder_to_filter)
+        result = self.test_instance.apply_filters(self.mutations_builder_to_filter)
 
         self.assertEqual(len(result), 2)
---
+++ tests/filter/test_filter_validator.py
@@ -0,0 +1,52 @@
+import unittest
+
+from filter.edit_GG_in_PAM_filter import EditGGInPAMFilter
+from filter.filter_validator import FilterValidator
+from filter.minimum_edits_filter import MinimumEditsFilter
+
+
+class TestFilterValidator(unittest.TestCase):
+
+    def test_validate_filters_when_no_filters(self):
+        no_filters = {}
+
+        result = FilterValidator(no_filters).validated_filters()
+
+        self.assertEqual(result, [])
+
+    def test_validate_filters_when_not_valid_filters(self):
+        not_valid_filters = {'filters': {'no_valid_key': 3}}
+
+        result = FilterValidator(not_valid_filters).validated_filters()
+
+        self.assertEqual(result, [])
+
+    def test_validate_filters_minimum_edit_filter(self):
+        minimum_edit_filter = {'filters': {'min_edits_allowed': 3}}
+
+        result = FilterValidator(minimum_edit_filter).validated_filters()
+
+        self.assertEqual(len(result), 1)
+        self.assertIs(result[0], MinimumEditsFilter)
+
+    def test_validate_filters_NGG_edit_required_true(self):
+        minimum_edit_filter = {'filters': {'NGG_edit_required': True}}
+
+        result = FilterValidator(minimum_edit_filter).validated_filters()
+
+        self.assertEqual(len(result), 1)
+        self.assertIs(result[0], EditGGInPAMFilter)
+
+    def test_validate_filters_NGG_edit_required_false(self):
+        minimum_edit_filter = {'filters': {'NGG_edit_required': False}}
+
+        result = FilterValidator(minimum_edit_filter).validated_filters()
+
+        self.assertEqual(result, [])
+
+    def test_validate_filters_NGG_edit_required_with_not_valid_value(self):
+        minimum_edit_filter = {'filters': {'NGG_edit_required': "NO VALID VALUE"}}
+
+        result = FilterValidator(minimum_edit_filter).validated_filters()
+
+        self.assertEqual(result, [])
