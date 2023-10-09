2126ef6cc230c955594849a9622990505709660e
Anna Shtanova <as63@sanger.ac.uk>
2023-10-09 14:18:46 +0000

    td-467TD-467 Add not_contain_TTTT+ filter

--- src/filter/filter_validator.py
+++ src/filter/filter_validator.py
@@ -2,6 +2,7 @@ from typing import List
 
 from abstractions.filter import Filter
 from filter.edit_GG_in_PAM_filter import EditGGInPAMFilter
+from filter.not_contain_TTTT_filter import NotContainTTTTFilter
 from filter.minimum_edits_filter import MinimumEditsFilter
 
 
@@ -13,8 +14,19 @@ class FilterValidator:
         valid_filters = []
         if self._filters:
             for key, value in self._filters.items():
-                if key == "min_edits_allowed" and type(value) is int:
-                    valid_filters.append(MinimumEditsFilter)
-                if key == 'NGG_edit_required' and value is True:
-                    valid_filters.append(EditGGInPAMFilter)
+                if key == 'min_edits_allowed':
+                    if isinstance(value, int):
+                        valid_filters.append(MinimumEditsFilter)
+                    else:
+                        print('Invalid value: the value given for minimum edits is not integer')
+                if key == 'NGG_edit_required':
+                    if value is True:
+                        valid_filters.append(EditGGInPAMFilter)
+                    else:
+                        print('Invalid value: the value given for NGG edits is not \'true\'')
+                if key == 'not_contain_TTTT+':
+                    if value is True:
+                        valid_filters.append(NotContainTTTTFilter)
+                    else:
+                        print('Invalid value: the value given for TTTT+ filter is not \'true\'')
         return valid_filters
