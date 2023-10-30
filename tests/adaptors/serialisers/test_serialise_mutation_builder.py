7b88b84e5d1810bd6cf185b3f56e014c3aa077aa
pk8 <pk8@sanger.ac.uk>
2023-10-30 09:54:02 +0000

    Indent issue

--- tests/adaptors/serialisers/test_serialise_mutation_builder.py
+++ tests/adaptors/serialisers/test_serialise_mutation_builder.py
@@ -39,6 +39,104 @@ class MutatorBuilderSerialiserTestCase(unittest.TestCase):
             'splice_mask_distance': 5,
         }
 
+    def test_convert_mutation_builders_to_df(self):
+        self.maxDiff = None
+        expected_columns = [
+            'guide_id', 'chromosome', 'cds_strand', 'gene_name',
+            'guide_strand', 'guide_start', 'guide_end', 'ot_summary',
+            'target_region_id', 'wge_percentile', 'codon_details'
+        ]
+
+        expected_codon_details = [[
+            {
+                'window_pos': 1,
+                'pos': 123,
+                'ref_codon': 'TCA',
+                'ref_pos_three': 'A',
+                'alt': 'G',
+                'lost_amino_acids': 'N/A',
+                'permitted': False,
+            },
+            {
+                'window_pos': 2,
+                'pos': 122,
+                'ref_codon': 'TCC',
+                'ref_pos_three': 'C',
+                'alt': 'T',
+                'lost_amino_acids': 'N/A',
+                'permitted': True,
+            }
+        ]]
+
+        df = convert_mutation_builders_to_df([self.mutation_builder], self.config)
+        actual_codon_details = df['codon_details'].tolist()
+
+        self.assertEqual(list(df.columns), expected_columns)
+        self.assertEqual(actual_codon_details, expected_codon_details)
+
+    def test_extract_codon_details(self):
+        expected_codon_details = [
+            {
+                'window_pos': 1,
+                'pos': 123,
+                'ref_codon': 'TCA',
+                'ref_pos_three': 'A',
+                'alt': 'G',
+                'lost_amino_acids': 'N/A',
+                'permitted': False,
+            },
+            {
+                'window_pos': 2,
+                'pos': 122,
+                'ref_codon': 'TCC',
+                'ref_pos_three': 'C',
+                'alt': 'T',
+                'lost_amino_acids': 'N/A',
+                'permitted': True,
+            }
+        ]
+
+        codon_details = extract_codon_details(self.mutation_builder, self.config)
+
+        self.assertEqual(codon_details, expected_codon_details)
+
+    def test_get_mutator_row(self):
+        expected_row = {
+            'guide_id': '123',
+            'chromosome': '1',
+            'cds_strand': "+",
+            'gene_name': 'ACT',
+            'guide_strand': "+",
+            'guide_start': 160,
+            'guide_end': 170,
+            'ot_summary': {0: 1, 1: 0, 2: 0, 3: 4, 4: 76},
+            'target_region_id': '101',
+            'wge_percentile': 25,
+        }
+
+        mutator_row = _get_mutator_row(self.mutation_builder)
+
+        self.assertEqual(mutator_row, expected_row)
+
+    def test_get_codon_row(self):
+        expected_row = {
+            'window_pos': 1,
+            'pos': 123,
+            'ref_codon': 'TCA',
+            'ref_pos_three': 'A',
+            'alt': 'G',
+            'lost_amino_acids': 'N/A',
+            'permitted': False,
+        }
+
+        cds_start = 100
+        cds_end = 200
+        codon = WindowCodon(bases='TCA', third_base_coord=123, third_base_pos=1, is_positive_strand=True)
+
+        codon_row = _get_codon_row(cds_start, cds_end, codon, self.config)
+
+        self.assertEqual(codon_row, expected_row)
+
     def test_serialise_mutation_builder_when_no_filter_applied(self):
         # fmt: off
         expected_serialisation = [{
