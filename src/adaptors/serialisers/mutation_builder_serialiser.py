94c5174943d5b5544e50c69f7f40dcf685c797dc
Anna Shtanova <as63@sanger.ac.uk>
2023-11-07 10:28:54 +0000

    td-466TD-466 Separate dataframe serialization

--- src/adaptors/serialisers/mutation_builder_serialiser.py
+++ src/adaptors/serialisers/mutation_builder_serialiser.py
@@ -24,37 +24,6 @@ def serialise_mutation_builder(
     return serialised_mutation_builder
 
 
-def convert_mutation_builders_to_df(mutation_builders: List[MutationBuilder], config: dict) -> pd.DataFrame:
-    if mutation_builders is None:
-        raise ValueError("Mutation builders not available for dataframing.")
-    data = []
-    for mb in mutation_builders:
-        mutation_builders_dict = _get_mutation_builder_dict(mb)
-        mutation_builders_dict["codon_details"] = extract_codon_details(mb, config)
-        data.append(mutation_builders_dict)
-
-    return pd.DataFrame(data)
-
-
-def count_valid_codons(mutation_builder: MutationBuilder) -> int:
-    return len(mutation_builder.codons)
-
-
-def _get_mutator_row(mutation_builder: MutationBuilder) -> dict:
-    return {
-        'target_region_id': mutation_builder.guide.target_region_id,
-        'guide_id': mutation_builder.guide.guide_id,
-        'centrality': mutation_builder.guide.centrality_score,
-        'wge_percentile': mutation_builder.guide.wge_percentile,
-        'valid_edits': count_valid_codons(mutation_builder),
-        'on_target_score': mutation_builder.guide.on_target_score if mutation_builder.guide.on_target_score else 'N/A',
-        'chromosome': mutation_builder.cds.chromosome,
-        'guide_start': mutation_builder.guide.start,
-        'guide_end': mutation_builder.guide.end,
-        'guide_strand': mutation_builder.guide.strand_symbol,
-    }
-
-
 def _get_mutation_builder_dict(mutation_builder: MutationBuilder, filter_applied: str = None) -> dict:
     return {
         'target_region_id': mutation_builder.guide.target_region.id,
@@ -84,6 +53,37 @@ def _get_codon_dict(cds_start: int, cds_end: int, codon: WindowCodon, config: di
         'alt': codon.edited_bases[2],
         'lost_amino_acids': lost_amino,
         'permitted': codon.is_edit_permitted(config, cds_start, cds_end),
+    }
+
+
+# Dataframe
+def convert_mutation_builders_to_df(mutation_builders: List[MutationBuilder], config: dict) -> pd.DataFrame:
+    if mutation_builders is None:
+        raise ValueError("Mutation builders not available for dataframing.")
+    data = []
+    for mb in mutation_builders:
+        row = _get_mutator_row(mb)
+        data.append(row)
+
+    return pd.DataFrame(data)
+
+
+def count_valid_codons(mutation_builder: MutationBuilder) -> int:
+    return len(mutation_builder.codons)
+
+
+def _get_mutator_row(mutation_builder: MutationBuilder) -> dict:
+    return {
+        'target_region_id': mutation_builder.guide.target_region.id,
+        'guide_id': mutation_builder.guide.guide_id,
+        'centrality': mutation_builder.guide.centrality_score,
+        'wge_percentile': mutation_builder.guide.wge_percentile,
+        'valid_edits': count_valid_codons(mutation_builder),
+        'on_target_score': mutation_builder.guide.on_target_score if mutation_builder.guide.on_target_score else 'N/A',
+        'chromosome': mutation_builder.cds.chromosome,
+        'guide_start': mutation_builder.guide.start,
+        'guide_end': mutation_builder.guide.end,
+        'guide_strand': mutation_builder.guide.strand_symbol,
     }
 
 
