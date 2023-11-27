4669b6eea9d17974a76f814f83c033a1095a5d31
Miguel Rada <mr33@sanger.ac.uk>
2023-11-27 09:55:09 +0000

    Td 468/cli options to json config file 1

--- src/cli.py
+++ src/cli.py
@@ -1,58 +1,58 @@
 from typing import List
 
+from config.config import Config
 from guide import GuideSequence
 from mutator.mutator import Mutator
 from retriever.retriever import Retriever
 from utils.arguments_parser import InputArguments
-from utils.config import prepare_config
 
 
 def main() -> None:
     parsed_input = InputArguments()
     args = parsed_input.arguments
-    config = prepare_config(args['conf'])
+    config = Config(args['conf'], args)
     command = parsed_input.command
 
-    resolve_command(command, args, config)
+    resolve_command(command, config)
 
 
-def resolve_command(command: str, args: dict, config: dict) -> None:
+def resolve_command(command: str, config: Config) -> None:
     if command == "mutator":
-        run_mutator_cmd(args, config)
+        run_mutator_cmd(config)
     if command == "retrieve":
-        run_retrieve_cmd(args, config)
+        run_retrieve_cmd(config)
     if command == "guide_selector":
-        run_guide_selector_cmd(args, config)
+        run_guide_selector_cmd(config)
 
 
-def run_guide_selector_cmd(args: dict, config: dict) -> None:
-    guide_sequences = run_retrieve_cmd(args, config)
+def run_guide_selector_cmd(config: Config) -> None:
+    guide_sequences = run_retrieve_cmd(config)
 
-    run_mutator_cmd(args, config, guide_sequences)
+    run_mutator_cmd(config, guide_sequences)
 
 
-def run_retrieve_cmd(args: dict, config: dict) -> List[GuideSequence]:
-    retriever = Retriever(config)
+def run_retrieve_cmd(config: Config) -> List[GuideSequence]:
+    retriever = Retriever(config.config_dict)
     print('Run retrieve command with config:', config)
 
     # Read Input Files
-    retriever.read_inputs(args)
+    retriever.read_inputs(config.args)
 
     # Run retriever
     retriever.run()
 
     # Write Output Files
-    retriever.write_outputs(output_dir=args['out_dir'])
+    retriever.write_outputs(output_dir=config.args['out_dir'])
 
     return retriever.guide_sequences
 
 
-def run_mutator_cmd(args: dict, config: dict, guide_sequences: List[GuideSequence] = None) -> None:
-    mutator = Mutator(config)
+def run_mutator_cmd(config: Config, guide_sequences: List[GuideSequence] = None) -> None:
+    mutator = Mutator(config.config_dict)
     print('Running PAM & Protospacer guide_selector')
 
     # Read Input Files
-    mutator.read_inputs(args, guide_sequences)
+    mutator.read_inputs(config.args, guide_sequences)
 
     # Run mutator
     mutator.run()
@@ -60,7 +60,7 @@ def run_mutator_cmd(args: dict, config: dict, guide_sequences: List[GuideSequenc
     print("Length of failed_mutations list:", len(mutator.failed_mutations))
 
     # Write Output Files
-    mutator.write_outputs(output_dir=args['out_dir'])
+    mutator.write_outputs(output_dir=config.args['out_dir'])
 
 
 if __name__ == '__main__':
