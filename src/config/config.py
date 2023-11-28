43ba31551d036dd28cb0d9565d3987e035d3692b
Miguel Rada <mr33@sanger.ac.uk>
2023-11-28 14:00:59 +0000

    Td 468/cli options to json config file 2

--- src/config/config.py
+++ src/config/config.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
+from utils.arguments_parser import InputArguments
 from utils.file_system import parse_json
 
 DEFAULT_CONFIG_FILE = Path(__file__).parent / '../../config/default_config.json'
 
 
 class Config:
-    def __init__(self, config_file: str, cli_args: dict):
-        self.config_dict = prepare_config(config_file)
-        self.args = cli_args
+
+    def __init__(self, input_args: InputArguments):
+        self.args = input_args.arguments
+        self.config_dict = prepare_config(self.args['conf'])
+        self.command = input_args.command
 
 
 def prepare_config(config_file: str) -> dict:
